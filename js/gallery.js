d8ee2ef916c6853557ee30ec4b6d6da417925baf
Romenig Lima Damasio <romenigld@gmail.com>
2018-09-11 01:11:27 +0200

    11.2) Changing the gallery image

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,13 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    });
+  });
 }
