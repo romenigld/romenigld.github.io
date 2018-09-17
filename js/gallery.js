b1e43b1dfc8c74431a289f05f18e716e6d1c0270
Romenig Lima Damasio <romenigld@gmail.com>
2018-09-17 22:45:43 +0200

    11.3) Setting an image as current

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,18 @@
 function activateGallery() {
-  let thumbnails = document.querySelector("#gallery-thumbs").
-                            querySelectorAll("img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
       let newImageSrc = thumbnail.dataset.largeVersion;
-      let newImageAlt = thumbnail.dataset.title;
       mainImage.setAttribute("src", newImageSrc);
+
+      let newImageAlt = thumbnail.dataset.title;
       mainImage.setAttribute("alt", newImageAlt);
+
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
     });
   });
 }
