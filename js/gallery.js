649555f86be89c23fbc065937dd0bb3c3a9b9b79
Romenig Lima Damasio <romenigld@gmail.com>
2018-09-17 23:56:58 +0200

    11.4) Changing the image info

--- js/gallery.js
+++ js/gallery.js
@@ -4,15 +4,24 @@ function activateGallery() {
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
-      // Set clicked image as main image.
+      // Set clicked image as display image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
 
       let newImageAlt = thumbnail.dataset.title;
       mainImage.setAttribute("alt", newImageAlt);
 
+      // Change which image is current.
       document.querySelector(".current").classList.remove("current");
       thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
